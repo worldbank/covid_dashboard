33852ada2c6c7aaa68452e394c6ea851489b93b6
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-03-19 09:07:26 -0400

    break down types of packages

--- 00_setup.R
+++ 00_setup.R
@@ -1,19 +1,15 @@
 
 #--------- make sure everyvody has the required packages
 
+#CRAN packages
 pkg_needed <- c("flexdashboard",
                 "WDI",
-                "wbgdata",
                 "plotly",
                 "shiny",
                 "reshape2",
                 "readxl",
                 "treemap",
-                "d3treeR",
                 "Hmisc",
-                "wbgcharts",
-                "wbgmaps",
-                "wbggeo",
                 "leaflet")
 
 a <- pkg_needed  %in% installed.packages()[,1]
@@ -21,4 +17,28 @@ pkg_toinstall <- pkg_needed[!a]  # packages not avaialble in pc
 if (length(pkg_toinstall) > 0) {
   install.packages(pkg_toinstall)
 }
+
+lapply(pkg_needed, library, character.only = TRUE)
+
+# WB packages
+pkg_needed <- c("wbgdata",
+                "wbgcharts",
+                "wbgmaps",
+                "wbggeo"
+                )
+
+a <- pkg_needed  %in% installed.packages()[,1]
+pkg_toinstall <- pkg_needed[!a]  # packages not avaialble in pc
+for (i in seq_along(pkg_toinstall)) {
   
+  devtools::install_github("worldbank/wbgviz", 
+                           subdir = pkg_toinstall[i])
+}
+lapply(pkg_needed, library, character.only = TRUE)
+
+# third party packages
+if (!("d3treeR" %in% installed.packages()[,1])) {
+  devtools::install_github("timelyportfolio/d3treeR")
+}
+library("d3treeR")
+
