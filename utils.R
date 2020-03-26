26274ea159b937bc61d20b8e3746e3824c2119ac
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-03-26 11:22:22 -0400

    New colors

--- utils.R
+++ utils.R
@@ -11,18 +11,34 @@ treemap_dat <- function(df,
 
 # Color palette -----------------------------------------------------------
 
-reg_palette <- c("#055bb3", "#fdb714", "#eb1c2d", "#872b90", "#00ab51", "#515151", "#e16a2d")
+regions <- c(
+  "South Asia",
+  "Sub-Saharan Africa",
+  "Europe & Central Asia",
+  "Middle East & North Africa",
+  "Latin America & Caribbean",
+  "North America",
+  "East Asia & Pacific"
+)
+
+# palette <- c("#055bb3", "#fdb714", "#eb1c2d", "#872b90", "#00ab51", "#515151", "#e16a2d")
+
+palette <- c("#5F4690", "#1D6996", "#38A6A5", "#0F8554", "#73AF48", "#EDAD08", 
+             "#855C75", "#D9AF6B", "#AF6458", "#736F4C", "#526A83", "#625377", 
+             "#66C5CC", "#F6CF71", "#F89C74", "#DCB0F2", "#87C55F", "#9EB9F3",
+             "#E17C05", "#CC503E", "#94346E", "#6F4070", "#994E95", "#666666",
+             "#68855C", "#9C9C5E", "#A06177", "#8C785D", "#467378", "#7C7C7C",
+             "#FE88B1", "#C9DB74", "#8BE0A4", "#B497E7", "#D3B484", "#B3B3B3")
+
+
+reg_palette <- palette[1:length(regions)]
+
+# scales::show_col(palette)
+# scales::show_col(reg_palette)
+
 reg_color   <- setNames(
   reg_palette,
-  c(
-    "South Asia",
-    "Sub-Saharan Africa",
-    "Europe & Central Asia",
-    "Middle East & North Africa",
-    "Latin America & Caribbean",
-    "North America",
-    "East Asia & Pacific"
-  )
+  regions
 )
 
 color_map <- c(
@@ -35,13 +51,7 @@ color_map <- c(
   `East Asia & Pacific`        = "#e16a2d"
 )
 
-color_map2 <- tribble(
-  ~Region                      , ~color,
-  "South Asia"                 , "#055bb3",
-  "Sub-Saharan Africa"         , "#fdb714",
-  "Europe & Central Asia"      , "#eb1c2d",
-  "Middle East & North Africa" , "#872b90",
-  "Latin America & Caribbean"  , "#00ab51",
-  "North America"              , "#515151",
-  "East Asia & Pacific"        , "#e16a2d",
+color_map2 <- tibble(
+  Region = regions, 
+  color  = reg_palette
 )
