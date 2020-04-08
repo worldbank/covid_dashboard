736c599f342bac0ec5c5d19d433b4339fc4ecafb
Tony Fujs <tfujs@worldbank.org>
2020-04-08 15:42:29 +0200

    Refactor code + add css loader framework

--- utils.R
+++ utils.R
@@ -55,3 +55,66 @@ color_map2 <- tibble(
   Region = regions, 
   color  = reg_palette
 )
+
+#' plot_cog_map
+#'
+#' @param mapdf data.frame: Map boundaries 
+#' @param cgdf data.frame: Center of Gravity data
+#' @param cg_x object: Variable to be mapped to x aesthetic
+#' @param cg_y object: Variable to be mapped to y aesthetic
+#' @param cg_size object: Variable to be mapped to size aesthetic
+#' @param cg_color object: Variable to be mapped to color aesthetic
+#' @param cg_tooltip object: Variable to be displayed by tooltip
+#' @param title character: Plot title
+#'
+#' @return ggplot
+#' @export
+#'
+plot_cog_map <- function(mapdf, 
+                         cgdf,
+                         cg_x,
+                         cg_y,
+                         cg_size,
+                         cg_color,
+                         cg_tooltip,
+                         title) {
+  p <- ggplot2::ggplot() +
+    ggplot2::geom_polygon(
+      data = mapdf,
+      ggplot2::aes(x = long, y = lat, group = group),
+      color = 'white',
+      size = 0.1,
+      fill = "grey"
+    ) +
+    ggiraph::geom_point_interactive(
+      data = cgdf,
+      ggplot2::aes(
+        x = {{cg_x}},
+        y = {{cg_y}},
+        size = {{cg_size}},
+        alpha = {{cg_color}},
+        color = {{cg_color}},
+        tooltip = paste("Value:", {{cg_tooltip}},
+                        "<br />Year:", {{cg_color}})
+      )
+    ) +
+    ggplot2::scale_colour_date(low = "#FF7878", high = "#780000") +
+    ggplot2::scale_size_continuous(range = c(1, 10)) +
+    ggplot2::labs(size = "Daily deaths", color = "Date") +
+    ggplot2::guides(alpha = FALSE, size = FALSE, color = FALSE) +
+    ggplot2::theme(
+      panel.background = ggplot2::element_rect(fill = 'white', color = 'white'),
+      legend.text = ggplot2::element_text(size = 14),
+      legend.title = ggplot2::element_text(size = 14),
+      axis.title = ggplot2::element_blank(),
+      axis.text = ggplot2::element_blank(),
+      axis.ticks = ggplot2::element_blank()
+    ) +
+    ggplot2::ggtitle(title) +
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(hjust = 0.5, face = "bold", size = 20),
+      legend.position = "bottom")
+  
+  return(p)
+}
+
